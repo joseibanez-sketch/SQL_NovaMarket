90ed02b1fc15ee624ff935c96eec3353b8160f73
Jose Manuel <joseibanez@unicomfacauca.edu.co>
2026-05-13 22:46:40 -0500

    sesion 08 y 09 entregada - consultas E1 E2 E3

--- E1-E2-E3.sql
+++ E1-E2-E3.sql
@@ -1,32 +1,4 @@
--- E3: (Difícil) Reproduce la tabla del dashboard de S4 completa: Ciudad, Ventas, Utilidad, Margen%. Con nombres reales.
-SELECT c.Ciudad,
-    COUNT(*) AS Transacciones,
-    ROUND(
-        SUM(
-            f.Precio_Venta * f.Cantidad * (1 - f.Descuento_Pct)
-        ),
-        2
-    ) AS Venta_Neta,
-    ROUND(SUM(f.Costo_Envio), 2) AS Costo_Envio_Total,
-    ROUND(
-        SUM(
-            f.Precio_Venta * f.Cantidad * (1 - f.Descuento_Pct) - f.Costo_Envio
-        ),
-        2
-    ) AS Utilidad,
-    ROUND(
-        SUM(
-            f.Precio_Venta * f.Cantidad * (1 - f.Descuento_Pct) - f.Costo_Envio
-        ) / SUM(
-            f.Precio_Venta * f.Cantidad * (1 - f.Descuento_Pct)
-        ) * 100,
-        2
-    ) AS Margen_Pct
-FROM FactVentas f
-    INNER JOIN DimCiudad c ON f.CiudadID = c.CiudadID
-GROUP BY c.Ciudad
-ORDER BY Utilidad DESC;
--- E1: (Fácil) Muestra nombre del producto, categoría y venta neta total de cada producto. Ordena de mayor a menor.
+--- E1: (Fácil) Muestra nombre del producto, categoría y venta neta total de cada producto. Ordena de mayor a menor.
 SELECT p.Producto,
     p.Categoria,
     ROUND(
